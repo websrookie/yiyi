450535b6a9c43739d391102f5a149055f81afba3
shaopeng <478221322@qq.com>
2024-09-13 13:53:49 +0800

    feat: 跑通vue3

---
+++ packages/yiyi-playground/vue2/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <YiyiImage
+      src="https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
+    ></YiyiImage>
+  </div>
+</template>
+
+<script>
+import YiyiUi from '@yiyi/ui';
+const { YiyiImage } = YiyiUi;
+
+export default {
+  name: 'App',
+  components: {
+    YiyiImage,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ packages/yiyi-ui/src/components/yiyi-image.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// import { ref } from 'vue';
+
+defineProps<{ src: string }>();
+</script>
+
+<template>
+  <img :src="src" class="image" />
+</template>
+
+<style scoped>
+.image {
+  width: 300px;
+  height: 400px;
+  margin: 0 auto;
+}
+</style>
