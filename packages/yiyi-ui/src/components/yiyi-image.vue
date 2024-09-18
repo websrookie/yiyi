205ddabf333aaabe1d361055f6d99cf30de3c477
websrookie <478221322@qq.com>
2024-09-18 11:27:30 +0800

    feat: vue-demi 调试

--- packages/yiyi-ui/src/components/yiyi-image.vue
+++ packages/yiyi-ui/src/components/yiyi-image.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-// import { ref } from 'vue';
+import { ref } from 'vue';
 
 defineProps<{ src: string }>();
+
+const value = ref(111);
+console.log(value);
 </script>
 
 <template>
