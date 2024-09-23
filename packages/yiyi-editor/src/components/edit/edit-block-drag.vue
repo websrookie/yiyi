682705abe3657e2fe4755ef79137d3c46e3a517a
websrookie <478221322@qq.com>
2024-09-23 13:58:50 +0800

    feat: 初步完善edit组件

---
+++ packages/yiyi-editor/src/components/edit/edit-block-drag.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="edit-block"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- packages/yiyi-editor/src/components/edit/edit-block.vue
+++ packages/yiyi-editor/src/components/edit/edit-block.vue
@@ -1,39 +1,106 @@
 <template>
-  <div>edit-block</div>
+  <div class="edit-block">
+    <div class="left">
+      <div
+        class="menu-item"
+        v-for="(item, index) in menuList"
+        :key="index"
+        :class="{ 'is-active': index === activeMenu }"
+        @click="activeMenu = index"
+      >
+        <v-icon
+          class="menu-icon"
+          :icon="index === activeMenu ? item.iconActive : item.icon"
+        ></v-icon>
+        <div class="menu-name">{{ item.name }}</div>
+      </div>
+    </div>
+    <div class="right">
+      <el-collapse v-mode="activeNames">
+        <el-collapse-item title="基础组件" name="1"></el-collapse-item>
+        <el-collapse-item title="高级组件" name="2"></el-collapse-item>
+      </el-collapse>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
 
-<style lang="scss" scoped>
-.edit-render {
-  container-type: inline-size;
-  position: relative;
-  width: 100%;
-  min-height: calc(100vh - var(--edit-header-height));
-  margin-left: var(--edit-block-width);
-  margin-top: var(--edit-header-height);
+const menuList = ref([
+  {
+    icon: 'block',
+    iconActive: 'blockActive',
+    name: '组件',
+  },
+  {
+    icon: 'kit',
+    iconActive: 'kitActive',
+    name: '套件',
+  },
+]);
+
+const activeMenu = ref(0);
+const activeNames = ref(['1', '2']);
+</script>
+
+<style scoped lang="scss">
+.edit-block {
+  position: fixed;
+  top: var(--edit-header-height);
+  left: 0;
+  flex-shrink: 0;
   background: white;
-  &.is-mobile {
-    width: 375px;
-    overflow: hidden;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: calc(var(--edit-header-height) + 20px);
-    margin-bottom: 20px;
-    transform: translateX(10px);
+  border-right: 1px solid var(--color-border);
+  display: flex;
+  justify-content: flex-start;
+  width: var(--edit-block-width);
+  .left {
+    width: 70px;
+    height: calc(100vh - var(--edit-header-height));
+    border-right: 1px solid var(--color-border);
+    .menu-item {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      padding: 5.5px 0;
+      border-radius: var(--border-radius);
+      cursor: pointer;
+      margin: 17px 5px;
+      &.is-active,
+      &:hover {
+        background: var(--color-block-hover);
+        border-radius: var(--border-radius);
+      }
+      .menu-icon {
+        width: 30px;
+        height: 30px;
+        margin: 0 auto;
+      }
+      .menu-name {
+        font-size: 14px;
+        line-height: 14px;
+        padding-top: 4px;
+      }
+    }
   }
-  .empty {
-    position: absolute;
-    z-index: 0;
-    top: 0;
-    top: 0;
-    width: inherit;
+  .right {
+    flex: 1;
+    height: calc(100vh - var(--edit-header-height));
+    overflow: auto;
   }
-  .render {
-    position: relative;
-    z-index: 1;
-    width: 100%;
-    height: 100%;
+  :deep(.el-collapse) {
+    border: 0;
+  }
+  :deep(.el-collapse-item__header) {
+    padding-left: 14px;
+    font-size: 14px;
+  }
+  :deep(.el-collapse-item__content) {
+    padding-left: 14px;
+    padding-right: 14px;
+    padding-bottom: 14px;
   }
 }
 </style>
